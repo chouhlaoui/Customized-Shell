564dd16ab9676cda6843f42ad4e499ccc7a8a6ed
chourouk <chouroukhalaoui@gmail.com>
2022-12-19 23:12:28 +0100

    test 3

---
+++ file.h
@@ -0,0 +1,15 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+//shih
+int takeInput(char* str);
+
+//mezelt naarach 
+int shell(char *readline);
+char *get_the_line(void);
+char **parse_the_line(char *buff);
+int execute_the_line(char *buuf[]);
+void sigintHandler(__attribute__((unused)) int sig_num);
