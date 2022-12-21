6730697b911560fa2dc45b979e697921bacc6221
chourouk <chouroukhalaoui@gmail.com>
2022-12-21 18:46:42 +0100

    test 5

--- final/file.h
+++ final/file.h
@@ -13,10 +13,3 @@ void takeInput(char* str); //lire la ligne de commande
 int DealWithFiles(FILE *fp); //open a file
 void printDir(); //afficher l'emplacement actuel
 int delimiterAvecEspace(char* str, char** parsed); //decouper la ligne de commande
-
-//mezelt naarach 
-int shell(char *readline);
-char *get_the_line(void);
-char **parse_the_line(char *buff);
-int execute_the_line(char *buuf[]);
-void sigintHandler(__attribute__((unused)) int sig_num);
