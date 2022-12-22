d78ca161183b37861d7153e79d95b7b68ec6b7cd
chourouk <chouroukhalaoui@gmail.com>
2022-12-22 04:58:27 +0100

    Test 8

--- final/main.c
+++ final/main.c
@@ -1,28 +1,52 @@
-//gcc -o Main -lreadline main.c file.c 
-
-#include "file.h"
+#include"file.h"
 #include<stdio.h>
 #include<string.h>
-
+#include<stdlib.h>
+#include<unistd.h>
+#include<sys/types.h>
+#include<sys/wait.h>
+#include<readline/readline.h>
+#include<readline/history.h>
 #define MAXCOM 1000 /* max number per line */ 
 #define MAXLIST 100 /* max number of commands to be supported */ 
 
-int main(void)
+
+int main (void)
 {
-    char* line;
-    int NbCmd;
-    char *parsedArgs[MAXLIST];
+int NbCmd;
+char inputString[MAXCOM], *parsedArgs[MAXLIST];
 
-    while (1)
+while (1)
+{
+    PrintDir();
+	TakeInput(inputString);
+    if(strcmp(inputString,"exit")!=0)
     {
-        PrintDir();
-        TakeInput(line);
-        NbCmd = DelimiterAvecEspace(strdup(line),parsedArgs);
- 	    ExecuteOneCommand(parsedArgs);
-    
-
+        NbCmd=DelimiterAvecEspace(strdup(inputString),parsedArgs);
+        if (PathHandler(parsedArgs)==0)
+        {
+            switch (Composee(inputString))
+            {
+            case 1:
+                printf(";");
+                break;
+            case 2:
+                printf("||");
+                break;
+            case 3:
+                printf("&&");
+                break;
+            default:
+                ExecuteOneCommand(parsedArgs);
+ 	            //fprintf(stderr, "Erreur %d\n", errno);
+                break;
+            }
+        }
     }
-    
-  return 0;
+    else 
+    {
+        break;
+    }
 }
-
+return 0;
+}
